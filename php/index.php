5b9c039a58505a15bad959bf74562dbc1b348700
unknown <mathispepin14170@gmail.com>
2020-11-03 09:57:31 +0100

    add files

---
+++ php/index.php
@@ -0,0 +1,7 @@
+<?php
+include_once "php/A-propos.php";
+include_once "php/Competance.php";
+include_once "php/Contact.php";
+include_once "php/Experience.php";
+include_once "php/Formation.php";
+?>
