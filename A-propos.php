85202c88ab178ca6766a1a7b0754d8d1e0aa136a
mathis-pepin <73874264+mathis-pepin@users.noreply.github.com>
2020-11-29 22:59:31 +0100

    Delete A-propos.php

--- A-propos.php
+++
@@ -1,2 +0,0 @@
-
-<p id="moi">Je m'appel Mathis PEPIN, j'ai 18ans, j'habite en Normandie. J'ai une passion pour les jeux vidéos et la technologie.<br>J'aime voyager et découvrir de nouvelles cultures</p>
