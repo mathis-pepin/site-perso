87bd8560029757d403becdac88cf4fca4079fcc3
mathis-pepin <73874264+mathis-pepin@users.noreply.github.com>
2020-11-30 13:54:34 +0100

    Delete A-propos.php

--- A-propos.php
+++
@@ -1,2 +0,0 @@
-
-<div id="container_moi"> <p id="moi">A propos de moi:<br><br><br>Je m'appelle Mathis PEPIN, j'ai 18ans, j'habite en Normandie. J'ai une passion pour les jeux vidéos et la technologie.<br>J'aime voyager et découvrir de nouvelles cultures. J'aimerais aller au Japon pour découvrir la culture typique de ce pays.</p><img src="assets/images/moi.jpg"></div>
