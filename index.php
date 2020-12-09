726c62c2afabd468d31628dab5ca2412b593ab16
mathis-pepin <73874264+mathis-pepin@users.noreply.github.com>
2020-12-09 22:27:27 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,25 @@
+<!DOCTYPE html>
+<html lang=fr>
+<head>
+  <title>Mon site</title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" type="text/css" href="mon-site.css"
+  <link rel="stylesheet" type="text/css" href="css/all.css"
+</head>
+<body>
 <?php
-include_once "src/monsite.php";
-include_once "src/mon-site.css";
+  include_once "src/monsite.php";
+  include_once "src/mon-site.css";
+  ?>
+  <?php
+  include_once "src/A-propos.php";
+  include_once "src/Experience.php";
+  include_once "src/Competances.php";
+  include_once "src/Formation.php";
+  include_once "src/Contact.php";
 ?>
-<?php
-include_once "src/A-propos.php";
-include_once "src/Experience.php";
-include_once "src/Competances.php";
-include_once "src/Formation.php";
-include_once "src/Contact.php";
-?>
+</body>
+</html>  
 
 
 
