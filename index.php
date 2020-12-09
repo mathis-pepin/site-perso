fe1c7e49aa7b78d559cceb724e6788f45e4ca01a
mathis-pepin <73874264+mathis-pepin@users.noreply.github.com>
2020-12-09 10:11:42 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
-include_once "/src/monsite.php";
+include_once "src/monsite.php";
 ?>
 <?php
-include_once "/src/A-propos.php";
-include_once "/src/Experience.php";
-include_once "/src/Competances.php";
-include_once "/src/Formation.php";
-include_once "/src/Contact.php";
+include_once "src/A-propos.php";
+include_once "src/Experience.php";
+include_once "src/Competances.php";
+include_once "src/Formation.php";
+include_once "src/Contact.php";
 ?>
